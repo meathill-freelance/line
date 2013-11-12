a4213f3bbc4ead1d8b7277d985566c7a0b263319
Meathill <meathill@gmail.com>
2013-11-13 01:24:35 +0800

    修改了大图 将文章和活动改成读取
    Signed-off-by: Meathill <meathill@gmail.com>

--- index.php
+++ index.php
@@ -11,9 +11,55 @@
 require(dirname(__FILE__) . '/inc/mustache.php');
 $tpl = new Mustache_Engine();
 
+$result = array(
+  'theme_url' => $home_url
+);
+
+// 公司新闻
+if (have_posts()) {
+  $blog = array();
+  $count = 0;
+  while (have_posts()) {
+    the_post();
+    $blog[] = array(
+      'title' => the_title_attribute(array('echo' => FALSE)),
+      'link' => apply_filters('the_permalink', get_permalink()),
+      'date' => apply_filters('the_time', get_the_time('Y-m-d'), 'Y-m-d'),
+    );
+    $count ++;
+    if ($count >= 2) {
+      break;
+    }
+  }
+  $result['blog'] = $blog;
+}
+
+// 最新活动
+$args = array(
+  'post_type' => 'page',
+  'post_parent' => 2,
+);
+$actives = new WP_Query($args);
+$count = 0;
+$result['actives'] = array();
+while ($actives->have_posts()) {
+  $actives->the_post();
+  $content = apply_filters('the_content', $content);
+  $result['actives'][] = array(
+    'thumbnail' => get_the_post_thumbnail(),
+    'title' => the_title('', '', FALSE),
+    'full_title' => the_title_attribute(array('echo' => FALSE)),
+    'link' => apply_filters('the_permalink', get_permalink()),
+    'date' => apply_filters('the_time', get_the_time('Y-m-d'), 'Y年m月d日'),
+    'excerpt' => apply_filters('the_excerpt', get_the_excerpt()),
+  );
+}
+
+// 最新产品
+
 $template = dirname(__FILE__) . '/template/index.html';
 $template = file_get_contents($template);
 $template = str_replace('"../', '"{{theme_url}}wp-content/themes/line/', $template);
 
 $home_url = esc_url(home_url('/'));
-echo $tpl->render($template, array('theme_url' => $home_url));
+echo $tpl->render($template, $result);
